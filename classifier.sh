b5785fb9663aec63cbb51e947f01b08cb042f3ca
glcanvas <duginetsn98@gmail.com>
2020-01-01 18:38:40 +0300

    merge with remove server

---
+++ classifier.sh
@@ -0,0 +1,16 @@
+~/nduginec_evn3/bin/python ~/ml-diplom/main_classifier.py --train_left 0 --train_right 500 --test_left 501 --test_right 2592 --description train500test_other_1
+~/nduginec_evn3/bin/python ~/ml-diplom/main_classifier.py --train_left 0 --train_right 500 --test_left 501 --test_right 2592 --description train500test_other_2
+~/nduginec_evn3/bin/python ~/ml-diplom/main_classifier.py --train_left 0 --train_right 500 --test_left 501 --test_right 2592 --description train500test_other_3
+~/nduginec_evn3/bin/python ~/ml-diplom/main_classifier.py --train_left 0 --train_right 500 --test_left 501 --test_right 2592 --description train500test_other_4
+
+~/nduginec_evn3/bin/python ~/ml-diplom/main_classifier.py --train_left 0 --train_right 1000 --test_left 1001 --test_right 2592 --description train1000test_other_1
+~/nduginec_evn3/bin/python ~/ml-diplom/main_classifier.py --train_left 0 --train_right 1000 --test_left 1001 --test_right 2592 --description train1000test_other_2
+~/nduginec_evn3/bin/python ~/ml-diplom/main_classifier.py --train_left 0 --train_right 1000 --test_left 1001 --test_right 2592 --description train1000test_other_3
+~/nduginec_evn3/bin/python ~/ml-diplom/main_classifier.py --train_left 0 --train_right 1000 --test_left 1001 --test_right 2592 --description train1000test_other_4
+~/nduginec_evn3/bin/python ~/ml-diplom/main_classifier.py --train_left 0 --train_right 1000 --test_left 1001 --test_right 2592 --description train1000test_other_5
+
+~/nduginec_evn3/bin/python ~/ml-diplom/main_classifier.py --train_left 0 --train_right 100 --test_left 101 --test_right 2592 --description train100test_other_1
+~/nduginec_evn3/bin/python ~/ml-diplom/main_classifier.py --train_left 0 --train_right 100 --test_left 101 --test_right 2592 --description train100test_other_2
+~/nduginec_evn3/bin/python ~/ml-diplom/main_classifier.py --train_left 0 --train_right 100 --test_left 101 --test_right 2592 --description train100test_other_3
+~/nduginec_evn3/bin/python ~/ml-diplom/main_classifier.py --train_left 0 --train_right 100 --test_left 101 --test_right 2592 --description train100test_other_4
+~/nduginec_evn3/bin/python ~/ml-diplom/main_classifier.py --train_left 0 --train_right 100 --test_left 101 --test_right 2592 --description train100test_other_5
