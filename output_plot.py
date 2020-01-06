eae704d34429bc838ca938edd0b3b2f28009f522
glcanvas <duginetsn98@gmail.com>
2020-01-06 13:33:00 +0300

    improve plot visualize

--- output_plot.py
+++ output_plot.py
@@ -1,9 +1,11 @@
 """draw plot of accuracy"""
 import matplotlib.pyplot as plt
 import os
+import re
+from sklearn.metrics import roc_curve, auc, precision_recall_curve, average_precision_score
 
 plot_images_path = "/home/nikita/PycharmProjects/ml-diplom/stat_plot"
-logs_path = "/home/nikita/PycharmProjects/ml-data/logs"
+logs_path = "/home/nikita/PycharmProjects/logs"
 
 
 def gain_train_parse(train_line):
@@ -37,7 +39,79 @@ def classifier_test_parse(test_line):
     return loss_cl, accuracy
 
 
-def draw_gain_plot(file_path: str):
+def __load_statistics(dct: dict) -> tuple:
+    f_measures = []
+    recall = []
+    precision = []
+    for i in range(0, 5):
+        key = 'f_1_{}'.format(i)
+        f_measures.append((key, dct[key]))
+    f_measures.append(('f_1_global', dct['f_1_global']))
+
+    for i in range(0, 5):
+        key = 'recall_{}'.format(i)
+        recall.append((key, dct[key]))
+    recall.append(('recall_global', dct['recall_global']))
+
+    for i in range(0, 5):
+        key = 'precision_{}'.format(i)
+        precision.append((key, dct[key]))
+    precision.append(('precision_global', dct['precision_global']))
+    return f_measures, recall, precision
+
+
+def __parse_auc(auc_value: str) -> tuple:
+    splited = re.split(r'trust_[0-5]{1}=|prob_[0-5]{1}=', auc_value)
+    splited = list(filter(lambda x: len(x) > 0, splited))
+    trust = []
+    prob = []
+    for i in range(0, 5):
+        trust.append(('trust_{}'.format(i), list(map(lambda x: int(float(x)), splited[i].split(",")))))
+    for i in range(0, 5):
+        prob.append(('prob_{}'.format(i), list(map(float, splited[5 + i].split(",")))))
+    return trust, prob
+
+
+def __to_dict(input: str) -> dict:
+    dct = dict()
+    for key_value in input.split():
+        idx = key_value.find("=")
+        if idx == -1:
+            continue
+        key = key_value[:idx]
+        value = key_value[idx + 1:]
+        dct[key] = value
+    return dct
+
+
+def gain_parse_test_and_classifier_all(input: str):
+    dct = __to_dict(input)
+    loss_cl = float(dct['Loss_CL'])
+    accuracy_cl = float(dct['Accuracy_CL_Percent']) if 'Accuracy_CL_Percent' in dct else float(dct['Accuracy_CL'])
+    f_measures, recall, precision = __load_statistics(dct)
+    if not 'auc_roc' in dct:
+        return loss_cl, accuracy_cl, f_measures, recall, precision
+    else:
+        trust, prob = __parse_auc(dct['auc_roc'])
+        return loss_cl, accuracy_cl, f_measures, recall, precision, trust, prob
+
+
+def gain_parse_train(input: str):
+    dct = __to_dict(input)
+    Loss_CL = float(dct['Loss_CL'])
+    Loss_AM = float(dct['Loss_AM'])
+    Loss_E = float(dct['Loss_E'])
+    Loss_Total = float(dct['Loss_Total'])
+    Accuracy_CL = float(dct['Accuracy_CL'][1:])
+    f_measures, recall, precision = __load_statistics(dct)
+    if not 'auc_roc' in dct:
+        return Loss_CL, Loss_AM, Loss_E, Loss_Total, Accuracy_CL, f_measures, recall, precision
+    else:
+        trust, prob = __parse_auc(dct['auc_roc'])
+        return Loss_CL, Loss_AM, Loss_E, Loss_Total, Accuracy_CL, f_measures, recall, precision, trust, prob
+
+
+def __load_file(file_path: str, train_parse, test_parse):
     train = []
     test = []
     train_index = 1
@@ -47,14 +121,227 @@ def draw_gain_plot(file_path: str):
     with open(file_path) as f:
         for l in f.readlines():
             if "TRAIN" in l:
-                train.append(gain_train_parse(l))
+                train.append(train_parse(l))
                 train_indexes.append(train_index)
                 train_index += 1
             elif "TEST" in l:
-                test.append(gain_test_parse(l))
+                test.append(test_parse(l))
                 test_indexes.append(test_index)
                 test_index += 4
         file_name = os.path.basename(f.name)
+    if len(train) == 0 or len(test) == 0:
+        raise ValueError("Empty data")
+    return train, test, train_indexes, test_indexes, file_name
+
+
+def draw_metrics_classification_plot(file_path: str):
+    train, test, train_indexes, test_indexes, file_name = __load_file(file_path, gain_parse_test_and_classifier_all,
+                                                                      gain_parse_test_and_classifier_all)
+    train_auc = 0
+    test_auc = 0
+    for i in train:
+        if len(i) > 5:
+            train_auc += 1
+    for i in test:
+        if len(i) > 5:
+            test_auc += 1
+
+    fig, axes = plt.subplots(5 + max(train_auc, test_auc) * 2, 2, figsize=(15, 50))
+    fig.tight_layout()
+    plt.subplots_adjust(bottom=0.5, top=2)
+
+    def __set_train_axes(axes_index: int, label_text: str, item_index: int):
+        axes[axes_index][0].set_title(label_text)
+        axes[axes_index][0].plot(train_indexes, list(map(lambda x: x[item_index], train)))
+
+    def __set_test_axes(axes_index: int, label_text: str, item_index: int):
+        axes[axes_index][0].set_title(label_text)
+        axes[axes_index][0].plot(test_indexes, list(map(lambda x: x[item_index], test)))
+
+    def __set_train_measures(axes_index: int, label_text: str, item_index: int):
+        axes[axes_index][1].set_title(label_text)
+        for i in range(0, 6):
+            legend = train[0][item_index][i][0]
+            axes[axes_index][1].plot(train_indexes, list(map(lambda x: x[item_index][i][1], train)), label=legend)
+        axes[axes_index][1].legend(loc='upper left')
+
+    def __set_test_measures(axes_index: int, label_text: str, item_index: int):
+        axes[axes_index][1].set_title(label_text)
+        for i in range(0, 6):
+            legend = test[0][item_index][i][0]
+            axes[axes_index][1].plot(test_indexes, list(map(lambda x: x[item_index][i][1], test)), label=legend)
+        axes[axes_index][1].legend(loc='upper left')
+
+    __set_train_axes(0, "train classification loss", 0)
+    __set_train_axes(1, "train accuracy", 1)
+    __set_train_measures(0, "f_1_train", 2)
+    __set_train_measures(1, "recall_train", 3)
+    __set_train_measures(2, "precision_train", 4)
+
+    __set_test_axes(2, "test classification loss", 0)
+    __set_test_axes(3, "test accuracy", 1)
+    __set_test_measures(3, "f_1_test", 2)
+    __set_test_measures(4, "recall_test", 3)
+    __set_test_measures(5, "precision_test", 4)
+
+    train_idx = 0
+    for idx, values in enumerate(train):
+        if len(values) <= 5:
+            continue
+        axes[5 + train_idx * 2][0].set_title("auc plot for train={}".format(idx + 1))
+        axes[5 + train_idx * 2 + 1][0].set_title("auc plot for train={}".format(idx + 1))
+        for i in range(0, 5):
+            trust_name, trust_value = values[5][i]
+            prob_name, prob_value = values[6][i]
+            a, b, _ = roc_curve(trust_value, prob_value)
+            c = auc(a, b)
+            axes[5 + train_idx * 2][0].plot(a, b, lw=2, label='ROC curve {} (area = {:.2f})'.format(prob_name[4:], c))
+            a1, b1, _ = precision_recall_curve(trust_value, prob_value)
+            c1 = average_precision_score(trust_value, prob_value)
+            axes[5 + train_idx * 2 + 1][0].plot(a1, b1, lw=2,
+                                                label='PR curve {} (area = {:.2f})'.format(prob_name[4:], c1))
+        axes[5 + train_idx * 2][0].legend(loc='upper left')
+        axes[5 + train_idx * 2 + 1][0].legend(loc='upper left')
+        train_idx += 1
+
+    test_idx = 0
+    for idx, values in enumerate(test):
+        if len(values) <= 5:
+            continue
+        axes[5 + test_idx * 2][1].set_title("auc plot for test={}".format((idx + 1) * 4))
+        axes[5 + test_idx * 2 + 1][1].set_title("auc plot for test={}".format((idx + 1) * 4))
+        for i in range(0, 5):
+            trust_name, trust_value = values[5][i]
+            prob_name, prob_value = values[6][i]
+            a, b, _ = roc_curve(trust_value, prob_value)
+            c = auc(a, b)
+            axes[5 + test_idx * 2][1].plot(a, b, lw=2, label='ROC curve {} (area = {:.2f})'.format(prob_name[4:], c))
+            a1, b1, _ = precision_recall_curve(trust_value, prob_value)
+            c1 = average_precision_score(trust_value, prob_value)
+            axes[5 + test_idx * 2 + 1][1].plot(a1, b1, lw=2,
+                                               label='PR curve {} (area = {:.2f})'.format(prob_name[4:], c1))
+        axes[5 + test_idx * 2][1].legend(loc='upper left')
+        axes[5 + test_idx * 2 + 1][1].legend(loc='upper left')
+        test_idx += 1
+
+    for ax in axes[0:5][:].flat:
+        ax.set(xlabel='epoch', ylabel='value')
+
+    for ax in axes[6:][:].flat:
+        ax.set(xlabel='False Positive Rate', ylabel='True Positive Rate')
+
+    plt.suptitle(file_name, y=1.99)
+    plt.savefig(os.path.join(plot_images_path, file_name[:-4]), bbox_inches='tight')
+    plt.show()
+
+
+def draw_gain_metric_plot(file_path: str):
+    train, test, train_indexes, test_indexes, file_name = __load_file(file_path, gain_parse_train,
+                                                                      gain_parse_test_and_classifier_all)
+
+    train_auc = 0
+    test_auc = 0
+    for i in train:
+        if len(i) > 8:
+            train_auc += 1
+    for i in test:
+        if len(i) > 5:
+            test_auc += 1
+
+    fig, axes = plt.subplots(7 + max(train_auc, test_auc) * 2, 2, figsize=(15, 50))
+    fig.tight_layout()
+    plt.subplots_adjust(bottom=0.5, top=2)
+
+    def __set_train_axes(axes_index: int, label_text: str, item_index: int):
+        axes[axes_index][0].set_title(label_text)
+        axes[axes_index][0].plot(train_indexes, list(map(lambda x: x[item_index], train)))
+
+    def __set_test_axes(axes_index: int, label_text: str, item_index: int):
+        axes[axes_index][0].set_title(label_text)
+        axes[axes_index][0].plot(test_indexes, list(map(lambda x: x[item_index], test)))
+
+    def __set_train_measures(axes_index: int, label_text: str, item_index: int):
+        axes[axes_index][1].set_title(label_text)
+        for i in range(0, 6):
+            legend = train[0][item_index][i][0]
+            axes[axes_index][1].plot(train_indexes, list(map(lambda x: x[item_index][i][1], train)), label=legend)
+        axes[axes_index][1].legend(loc='upper left')
+
+    def __set_test_measures(axes_index: int, label_text: str, item_index: int):
+        axes[axes_index][1].set_title(label_text)
+        for i in range(0, 6):
+            legend = test[0][item_index][i][0]
+            axes[axes_index][1].plot(test_indexes, list(map(lambda x: x[item_index][i][1], test)), label=legend)
+        axes[axes_index][1].legend(loc='upper left')
+
+    __set_train_axes(0, "train classification loss", 0)
+    __set_train_axes(1, "train am loss", 1)
+    __set_train_axes(2, "train e loss", 2)
+    __set_train_axes(3, "train total loss", 3)
+    __set_train_axes(4, "train accuracy", 4)
+    __set_train_measures(0, "f_1_train", 5)
+    __set_train_measures(1, "recall_train", 6)
+    __set_train_measures(2, "precision_train", 7)
+
+    __set_test_axes(5, "test classification loss", 0)
+    __set_test_axes(6, "test accuracy", 1)
+    __set_test_measures(3, "f_1_test", 2)
+    __set_test_measures(4, "recall_test", 3)
+    __set_test_measures(5, "precision_test", 4)
+
+    train_idx = 0
+    for idx, values in enumerate(train):
+        if len(values) <= 8:
+            continue
+        axes[7 + train_idx * 2][0].set_title("auc plot for train={}".format(idx + 1))
+        axes[7 + train_idx * 2 + 1][0].set_title("auc plot for train={}".format(idx + 1))
+        for i in range(0, 5):
+            trust_name, trust_value = values[8][i]
+            prob_name, prob_value = values[9][i]
+            a, b, _ = roc_curve(trust_value, prob_value)
+            c = auc(a, b)
+            axes[7 + train_idx * 2][0].plot(a, b, lw=2, label='ROC curve {} (area = {:.2f})'.format(prob_name[4:], c))
+            a1, b1, _ = precision_recall_curve(trust_value, prob_value)
+            c1 = average_precision_score(trust_value, prob_value)
+            axes[7 + train_idx * 2 + 1][0].plot(a1, b1, lw=2,
+                                                label='PR curve {} (area = {:.2f})'.format(prob_name[4:], c1))
+        axes[7 + train_idx * 2][0].legend(loc='upper left')
+        axes[7 + train_idx * 2 + 1][0].legend(loc='upper left')
+        train_idx += 1
+
+    test_idx = 0
+    for idx, values in enumerate(test):
+        if len(values) <= 5:
+            continue
+        axes[7 + test_idx * 2][1].set_title("auc plot for test={}".format((idx + 1) * 4))
+        axes[7 + test_idx * 2 + 1][1].set_title("auc plot for test={}".format((idx + 1) * 4))
+        for i in range(0, 5):
+            trust_name, trust_value = values[5][i]
+            prob_name, prob_value = values[6][i]
+            a, b, _ = roc_curve(trust_value, prob_value)
+            c = auc(a, b)
+            axes[7 + test_idx * 2][1].plot(a, b, lw=2, label='ROC curve {} (area = {:.2f})'.format(prob_name[4:], c))
+            a1, b1, _ = precision_recall_curve(trust_value, prob_value)
+            c1 = average_precision_score(trust_value, prob_value)
+            axes[7 + test_idx * 2 + 1][1].plot(a1, b1, lw=2,
+                                               label='PR curve {} (area = {:.2f})'.format(prob_name[4:], c1))
+        axes[7 + test_idx * 2][1].legend(loc='upper left')
+        axes[7 + test_idx * 2 + 1][1].legend(loc='upper left')
+        test_idx += 1
+
+    for ax in axes[0:7][:].flat:
+        ax.set(xlabel='epoch', ylabel='value')
+
+    for ax in axes[8:][:].flat:
+        ax.set(xlabel='False Positive Rate', ylabel='True Positive Rate')
+
+    plt.suptitle(file_name, y=1.99)
+    plt.savefig(os.path.join(plot_images_path, file_name[:-4]), bbox_inches='tight')
+    plt.show()
+
+
+def draw_gain_plot(file_path: str):
+    train, test, train_indexes, test_indexes, file_name = __load_file(file_path, gain_train_parse, gain_test_parse)
     fig, axes = plt.subplots(7, figsize=(9, 11))
     fig.tight_layout()
     plt.subplots_adjust(bottom=0.5, top=2)
@@ -83,23 +370,8 @@ def draw_gain_plot(file_path: str):
 
 
 def draw_classification_plot(file_path: str):
-    train = []
-    test = []
-    train_index = 1
-    test_index = 4
-    train_indexes = []
-    test_indexes = []
-    with open(file_path) as f:
-        for l in f.readlines():
-            if "TRAIN" in l:
-                train.append(classifier_train_parse(l))
-                train_indexes.append(train_index)
-                train_index += 1
-            elif "TEST" in l:
-                test.append(classifier_test_parse(l))
-                test_indexes.append(test_index)
-                test_index += 4
-        file_name = os.path.basename(f.name)
+    train, test, train_indexes, test_indexes, file_name = __load_file(file_path, classifier_train_parse,
+                                                                      classifier_test_parse)
     fig, axes = plt.subplots(4, figsize=(9, 11))
     fig.tight_layout()
     plt.subplots_adjust(bottom=0.5, top=2)
@@ -127,93 +399,26 @@ def draw_classification_plot(file_path: str):
 def visualize_all():
     for r, d, f in os.walk(logs_path):
         for file in f:
-            if "classifier" in file:
-                draw_classification_plot(os.path.join(r, file))
+            try:
+                if "metric_classifier" in file:
+                    print("begin", file)
+                    draw_metrics_classification_plot(os.path.join(r, file))
+                    print("end", file)
+                elif "classifier" in file:
+                    print("begin", file)
+                    draw_classification_plot(os.path.join(r, file))
+                    print("end", file)
+                elif "metric_gain" in file:
+                    print("begin", file)
+                    draw_gain_metric_plot(os.path.join(r, file))
+                    print("end", file)
+                elif "gain" in file:
+                    print("begin", file)
+                    draw_gain_plot(os.path.join(r, file))
+                    print("end", file)
+            except ValueError as e:
+                print("empty file: {}".format(file))
 
-            elif "gain" in file:
-                draw_gain_plot(os.path.join(r, file))
 
-
-# visualize_all()
-
-# if __name__ == "__main__":
-#    visualize_all()
-from sklearn import metrics
-import numpy as np
-
-y = np.array([1, 1, 2, 2])
-scores = np.array([0.1, 0.4, 0.35, 0.8])
-fpr, tpr, thresholds = metrics.roc_curve(y, scores, pos_label=2)
-print(fpr, tpr, thresholds)
-
-import numpy as np
-import matplotlib.pyplot as plt
-from itertools import cycle
-
-from sklearn import svm, datasets
-from sklearn.metrics import roc_curve, auc
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import label_binarize
-from sklearn.multiclass import OneVsRestClassifier
-from scipy import interp
-from sklearn.metrics import roc_auc_score
-
-# Import some data to play with
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
-
-# Binarize the output
-y = label_binarize(y, classes=[0, 1, 2])
-n_classes = y.shape[1]
-
-# Add noisy features to make the problem harder
-random_state = np.random.RandomState(0)
-n_samples, n_features = X.shape
-X = np.c_[X, random_state.randn(n_samples, 200 * n_features)]
-
-# shuffle and split training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5,
-                                                    random_state=0)
-
-# Learn to predict each class against the other
-classifier = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True,
-                                         random_state=random_state))
-y_score = classifier.fit(X_train, y_train).decision_function(X_test)
-
-# Compute ROC curve and ROC area for each class
-fpr = dict()
-tpr = dict()
-roc_auc = dict()
-for i in range(n_classes):
-    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
-    roc_auc[i] = auc(fpr[i], tpr[i])
-
-# Compute micro-average ROC curve and ROC area
-fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
-roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-
-trust_0 = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
-           0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-           0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-prob_4 = [0.67969, 0.63364, 0.42932, 0.63385, 0.64212, 0.36453, 0.54794, 0.44479, 0.48420, 0.53724, 0.49006, 0.65804,
-          0.57631, 0.49099, 0.47839, 0.61472, 0.30450, 0.41202, 0.49871, 0.37486, 0.39097, 0.55319, 0.32127, 0.60204,
-          0.64038, 0.59119, 0.23944, 0.63146, 0.64349, 0.68464, 0.48382, 0.34811, 0.54935, 0.63747, 0.59903, 0.55663,
-          0.53202, 0.50537, 0.55123, 0.31934, 0.54358, 0.47011, 0.56575, 0.72461, 0.62780, 0.39381, 0.42562, 0.60204,
-          0.54649, 0.54091]
-
-a, b, _ = roc_curve(trust_0, prob_4)
-c = auc(a, b)
-
-plt.figure()
-lw = 2
-plt.plot(a, b, color='darkorange',
-         lw=lw, label='ROC curve (area = %0.2f)' % c)
-plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver operating characteristic example')
-plt.legend(loc="lower right")
-plt.show()
+if __name__ == "__main__":
+    visualize_all()
