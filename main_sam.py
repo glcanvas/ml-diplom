f14439c73f95fa57bce3cf1174c4b68deb56f302
glcanvas <duginetsn98@gmail.com>
2020-02-02 17:36:27 +0300

    sam runners

--- main_sam.py
+++ main_sam.py
@@ -11,52 +11,87 @@ import sam_train as st
 classes = 1
 
 if __name__ == "__main__":
-    P.initialize_log_name("_" + "AA")
-    loader = il.DatasetLoader.initial()
-    train_segments = loader.load_tensors(0, 3, 4)
-    train_classifier = loader.load_tensors(0, 3, 0)
-    test = loader.load_tensors(0, 4)
+    parsed = P.parse_input_commands().parse_args(sys.argv[1:])
+    gpu = int(parsed.gpu)
+    parsed_description = parsed.description
+    train_left = int(parsed.train_left)
+    train_right = int(parsed.train_right)
+    test_left = int(parsed.test_left)
+    test_right = int(parsed.test_right)
+    train_segments_count = int(parsed.segments)
+    pre_train = int(parsed.pre_train)
+    # use_am_loss = parsed.am_loss.lower() == "true"
+    # gradient_layer_name = parsed.gradient_layer_name
+    # from_gradient_layer = parsed.from_gradient_layer.lower() == "true"
+    epochs = int(parsed.epochs)
 
-    train_segments_set = DataLoader(il.ImageDataset(train_segments), batch_size=10, shuffle=True)
-    train_classifier_set = DataLoader(il.ImageDataset(train_classifier), batch_size=10, shuffle=True)
-    test_set = DataLoader(il.ImageDataset(test), batch_size=10)
+    description = "{}_train_left-{},train_segments-{},train_right-{},test_left-{},test_right-{},pre_train-{}" \
+        .format(parsed_description,
+                train_left,
+                train_segments_count,
+                train_right,
+                test_left,
+                test_right,
+                pre_train
+                )
 
-    sam_branch = nn.Sequential(
-        nn.Conv2d(64, 64, kernel_size=(3, 3)),
-        nn.ReLU(),
-        nn.Conv2d(64, 128, kernel_size=(3, 3)),
-        nn.ReLU(),
-        nn.MaxPool2d(kernel_size=2, stride=2),
-        nn.Conv2d(128, 64, kernel_size=(3, 3), padding=(1, 1)),
-        nn.ReLU(),
-        nn.MaxPool2d(kernel_size=2, stride=2, padding=(1, 1)),
-        nn.Conv2d(64, 128, kernel_size=(3, 3), padding=(1, 1)),
-        nn.ReLU(),
-        nn.Conv2d(128, 1, kernel_size=(3, 3), padding=(1, 1)),
-        nn.Sigmoid()
-    )
-    model = m.vgg16(pretrained=True)
-    basis_branch = model.features[:4]
+    P.initialize_log_name("_" + description)
 
-    # parallel
-    classifier_branch = model.features[4:16]
+    try:
+        loader = il.DatasetLoader.initial()
+        train_segments = loader.load_tensors(train_left, train_segments_count, train_segments_count)
+        train_classifier = loader.load_tensors(train_segments_count, train_right, 0)
+        test = loader.load_tensors(test_left, test_right)
 
-    merged_branch = model.features[16:]
-    avg_pool = model.avgpool
-    classifier = nn.Sequential(*model.classifier,
-                               nn.Linear(1000, classes),
-                               nn.Sigmoid())
+        train_segments_set = DataLoader(il.ImageDataset(train_segments), batch_size=10, shuffle=True)
+        train_classifier_set = DataLoader(il.ImageDataset(train_classifier), batch_size=10, shuffle=True)
+        test_set = DataLoader(il.ImageDataset(test), batch_size=10)
 
-    sam_model = ss.SAM(basis_branch,
-                       sam_branch,
-                       classifier_branch,
-                       merged_branch,
-                       avg_pool,
-                       classifier)
-    # print(sam_model)
-    sam_train = st.SAM_TRAIN(sam_model, train_classifier_set, train_segments_set, test_set, classes=classes,
-                             pre_train_epochs=2,
-                             train_epochs=15,
-                             save_train_logs_epochs=5,
-                             test_each_epoch=5)
-    sam_train.train()
+        sam_branch = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=(3, 3)),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=(3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 64, kernel_size=(3, 3), padding=(1, 1)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=(1, 1)),
+            nn.Conv2d(64, 128, kernel_size=(3, 3), padding=(1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(128, 1, kernel_size=(3, 3), padding=(1, 1)),
+            nn.Sigmoid()
+        )
+        model = m.vgg16(pretrained=True)
+        basis_branch = model.features[:4]
+
+        # parallel
+        classifier_branch = model.features[4:16]
+
+        merged_branch = model.features[16:]
+        avg_pool = model.avgpool
+        classifier = nn.Sequential(*model.classifier,
+                                   nn.Linear(1000, classes),
+                                   nn.Sigmoid())
+
+        sam_model = ss.SAM(basis_branch,
+                           sam_branch,
+                           classifier_branch,
+                           merged_branch,
+                           avg_pool,
+                           classifier)
+        # print(sam_model)
+        sam_train = st.SAM_TRAIN(sam_model, train_classifier_set, train_segments_set, test_set, classes=classes,
+                                 pre_train_epochs=pre_train,
+                                 gpu_device=gpu,
+                                 train_epochs=epochs,
+                                 save_train_logs_epochs=4,
+                                 test_each_epoch=4)
+        sam_train.train()
+
+    except BaseException as e:
+        print("EXCEPTION", e)
+        print(type(e))
+        P.write_to_log("EXCEPTION", e, type(e))
+        traceback.print_stack()
+
+        raise e
