adbf456e77969c6335b053ede6fd8844e26eb50d
glcanvas <duginetsn98@gmail.com>
2020-02-07 00:45:02 +0300

    sam honest split

--- run_sam_cl.sh
+++ run_sam_cl.sh
@@ -1,14 +1,14 @@
 source ~/nduginec_evn3/bin/activate
 
 GPU=1
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.5 --segments 0.4 --test 0.1  --pre_train 100 --epochs 100 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.5 --segments 0.4 --test 0.1  --pre_train 100 --epochs 100 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.1 --segments 0.8 --test 0.1  --pre_train 100 --epochs 100 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.1 --segments 0.8 --test 0.1  --pre_train 100 --epochs 100 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.1 --segments 0.5 --test 0.4  --pre_train 100 --epochs 100 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.1 --segments 0.5 --test 0.4  --pre_train 100 --epochs 100 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.25--segments 0.25 --test 0.5 --pre_train 100 --epochs 100 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.25--segments 0.25 --test 0.5 --pre_train 100 --epochs 100 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.1--segments 0.6 --test 0.3   --pre_train 100 --epochs 100 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.1--segments 0.6 --test 0.3   --pre_train 100 --epochs 100 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.5 --segments 0.4 --test 0.1  --pre_train 100 --epochs 100 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.5 --segments 0.4 --test 0.1  --pre_train 100 --epochs 100 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.1 --segments 0.8 --test 0.1  --pre_train 100 --epochs 100 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.1 --segments 0.8 --test 0.1  --pre_train 100 --epochs 100 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.1 --segments 0.5 --test 0.4  --pre_train 100 --epochs 100 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.1 --segments 0.5 --test 0.4  --pre_train 100 --epochs 100 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.25 --segments 0.25 --test 0.5 --pre_train 100 --epochs 100 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.25 --segments 0.25 --test 0.5 --pre_train 100 --epochs 100 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.1--segments 0.6 --test 0.3   --pre_train 100 --epochs 100 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_only_classif_honest --classifier 0.1--segments 0.6 --test 0.3   --pre_train 100 --epochs 100 --gpu $GPU
 
--- run_sam_segm.sh
+++ run_sam_segm.sh
@@ -1,15 +1,15 @@
 source ~/nduginec_evn3/bin/activate
 
 GPU=0
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.5 --segments 0.4 --test 0.1  --pre_train 10 --change_lr 10 --epochs 150 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.5 --segments 0.4 --test 0.1  --pre_train 10 --change_lr 10 --epochs 150 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.1 --segments 0.8 --test 0.1  --pre_train 30 --change_lr 10 --epochs 150 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.1 --segments 0.8 --test 0.1  --pre_train 30 --change_lr 10 --epochs 150 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.1 --segments 0.5 --test 0.4  --pre_train 30 --change_lr 5 --epochs 150 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.1 --segments 0.5 --test 0.4  --pre_train 30 --change_lr 5 --epochs 150 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.25--segments 0.25 --test 0.5 --pre_train 10 --change_lr 10 --epochs 150 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.25--segments 0.25 --test 0.5 --pre_train 10 --change_lr 10 --epochs 150 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.1--segments 0.6 --test 0.3   --pre_train 30 --change_lr 10 --epochs 150 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.1--segments 0.6 --test 0.3   --pre_train 30 --change_lr 10 --epochs 150 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.5 --segments 0.4 --test 0.1  --pre_train 30 --change_lr 5 --epochs 150 --gpu $GPU
-~/nduginec_evn3/bin/python ~/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.5 --segments 0.4 --test 0.1  --pre_train 30 --change_lr 5 --epochs 150 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.5 --segments 0.4 --test 0.1  --pre_train 10 --change_lr 10 --epochs 150 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.5 --segments 0.4 --test 0.1  --pre_train 10 --change_lr 10 --epochs 150 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.1 --segments 0.8 --test 0.1  --pre_train 30 --change_lr 10 --epochs 150 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.1 --segments 0.8 --test 0.1  --pre_train 30 --change_lr 10 --epochs 150 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.1 --segments 0.5 --test 0.4  --pre_train 30 --change_lr 5 --epochs 150 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.1 --segments 0.5 --test 0.4  --pre_train 30 --change_lr 5 --epochs 150 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.25 --segments 0.25 --test 0.5 --pre_train 10 --change_lr 10 --epochs 150 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.25 --segments 0.25 --test 0.5 --pre_train 10 --change_lr 10 --epochs 150 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.1--segments 0.6 --test 0.3   --pre_train 30 --change_lr 10 --epochs 150 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.1--segments 0.6 --test 0.3   --pre_train 30 --change_lr 10 --epochs 150 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.5 --segments 0.4 --test 0.1  --pre_train 30 --change_lr 5 --epochs 150 --gpu $GPU
+~/nduginec_evn3/bin/python ~/ml2/ml-diplom/main_sam.py --description sam_lr_l1_cbam_honest --classifier 0.5 --segments 0.4 --test 0.1  --pre_train 30 --change_lr 5 --epochs 150 --gpu $GPU
