307c96c0743f800a74b780051106d7fb86d07f0a
glcanvas <duginetsn98@gmail.com>
2020-02-08 13:00:33 +0200

    fix run

--- run_sam_cl.sh
+++ run_sam_cl.sh
@@ -1,11 +1,11 @@
-GPU=0
+GPU=1
 RUN_NAME=RUN_01
 ALGORITHM_NAME=SAM_LOSS
-SCRIPT_PATH=ml2/ml-diplom
+SCRIPT_PATH=ml1/ml-diplom
 SCRIPT_NAME=main_sam.py
 DESCRIPTION=N
 EPOCHS_COUNT=150
-PRE_TRAIN_EPOCHS_COUNT=150
+PRE_TRAIN=150
 CHANGE_LR=150
 
 ~/nduginec_evn3/bin/python ~/$SCRIPT_PATH/$SCRIPT_NAME --description $DESCRIPTION --run_name $RUN_NAME --algorithm_name $ALGORITHM_NAME --epochs $EPOCHS_COUNT --gpu $GPU --pre_train $PRE_TRAIN --change_lr $CHANGE_LR
