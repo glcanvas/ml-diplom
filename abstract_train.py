bba5475ab1ad85d49b96cc9b4b130c274a8bd412
glcanvas <duginetsn98@gmail.com>
2020-02-25 00:09:43 +0300

    rewrite runners

--- abstract_train.py
+++ abstract_train.py
@@ -313,3 +313,31 @@ class AbstractTrain:
         print("=" * 50)
         plt.savefig(os.path.join(self.snapshot_dir, snapshot_name))
         plt.close(fig)
+
+
+"""
+ginec@laplas:~/ml2/ml-diplom/runners_alternate_one_loss$ 
+nduginec@laplas:~/ml2/ml-diplom/runners_alternate_one_loss$ EXCEPTION CUDA out of memory. Tried to allocate 392.00 MiB (GPU 0; 10.92 GiB total capacity; 3.24 GiB already allocated; 216.69 MiB free; 3.33 GiB reserved in total by PyTorch)
+<class 'RuntimeError'>
+EXCEPTION
+CUDA out of memory. Tried to allocate 392.00 MiB (GPU 0; 10.92 GiB total capacity; 3.24 GiB already allocated; 216.69 MiB free; 3.33 GiB reserved in total by PyTorch)
+<class 'RuntimeError'>
+  File "/home/nduginec/ml2/ml-diplom/main_alternate.py", line 75, in <module>
+    traceback.print_stack()
+Traceback (most recent call last):
+  File "/home/nduginec/ml2/ml-diplom/main_alternate.py", line 77, in <module>
+    raise e
+  File "/home/nduginec/ml2/ml-diplom/main_alternate.py", line 69, in <module>
+    sam_train.train()
+  File "/home/nduginec/ml2/ml-diplom/alternate_attention_module_train.py", line 75, in train
+    self.train_segments_set)
+  File "/home/nduginec/ml2/ml-diplom/abstract_train.py", line 119, in train_classifier
+    classification_loss.backward()
+  File "/home/nduginec/nduginetc_env3/lib/python3.5/site-packages/torch/tensor.py", line 195, in backward
+    torch.autograd.backward(self, gradient, retain_graph, create_graph)
+  File "/home/nduginec/nduginetc_env3/lib/python3.5/site-packages/torch/autograd/__init__.py", line 99, in backward
+    allow_unreachable=True)  # allow_unreachable flag
+RuntimeError: CUDA out of memory. Tried to allocate 392.00 MiB (GPU 0; 10.92 GiB total capacity; 3.24 GiB already allocated; 216.69 MiB free; 3.33 GiB reserved in total by PyTorch)
+/home/nduginec/nduginetc_env3/lib/python3.5/site-packages/torchvision/transforms/transforms.py:220: UserWarning: The use of the transforms.Scale transform is deprecated, please use transforms.Resize instead.
+  "please use transforms.Resize
+"""
