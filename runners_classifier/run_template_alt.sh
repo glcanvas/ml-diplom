7a505d9da2c70cbb6620f45392ff02b02cba6700
glcanvas <duginetsn98@gmail.com>
2020-03-18 08:53:59 +0300

    fix dot product

--- runners_classifier/run_template_alt.sh
+++ runners_classifier/run_template_alt.sh
@@ -1,12 +1,12 @@
-ALGORITHM_NAME=VGG16
+ALGORITHM_NAME=TWO_LOSS_WITH_AM
 
-SCRIPT_PATH=ml2/ml-diplom
-SCRIPT_NAME=main_default_classifier.py
+SCRIPT_PATH=ml3/ml-diplom
+SCRIPT_NAME=main_alternate.py
 
-DESCRIPTION=BASE_LINE_CLASSIFIER_DEFAULT_LOSS
+DESCRIPTION=TWO_LOSSES_FAIR_BIG_AM_DEFAULT_LOSS_WITH_DOT
 
 EPOCHS_COUNT=150
-PRE_TRAIN=150
+PRE_TRAIN=20
 
 $EXECUTOR_NAME ~/$SCRIPT_PATH/$SCRIPT_NAME --description $DESCRIPTION --run_name $RUN_NAME --left_class_number $L_CN --right_class_number $R_CN --algorithm_name $ALGORITHM_NAME --epochs $EPOCHS_COUNT --gpu $GPU --pre_train $PRE_TRAIN --train_set $TRAIN_SET_SIZE
 $EXECUTOR_NAME ~/$SCRIPT_PATH/$SCRIPT_NAME --description $DESCRIPTION --run_name $RUN_NAME --left_class_number $L_CN --right_class_number $R_CN --algorithm_name $ALGORITHM_NAME --epochs $EPOCHS_COUNT --gpu $GPU --pre_train $PRE_TRAIN --train_set $TRAIN_SET_SIZE
