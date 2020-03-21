0227e96f1a5fbe4e17bd8cf53488668cc48c1cff
glcanvas <duginetsn98@gmail.com>
2020-03-22 00:47:10 +0300

    first attention work

--- main_first_attention.py
+++ main_first_attention.py
@@ -1,4 +1,4 @@
-import image_loader as il
+import voc_loader as vl
 from torch.utils.data import DataLoader
 import property as P
 import sys
@@ -16,20 +16,25 @@ if __name__ == "__main__":
     parsed_description = parsed.description
     pre_train = int(parsed.pre_train)
     train_set_size = int(parsed.train_set)
+    test_set_size = int(parsed.test_set)
     epochs = int(parsed.epochs)
     run_name = parsed.run_name
     algorithm_name = parsed.algorithm_name
+    voc_items = list(filter(lambda x: len(x) > 0, parsed.voc_items.split(",")))
 
-    description = "description-{},train_set-{},epochs-{}".format(
+    description = "description-{},train_set-{},test_set-{},epochs-{},voc-{}".format(
         parsed_description,
         train_set_size,
-        epochs
+        test_set_size,
+        epochs,
+        ",".join(voc_items)
     )
+
+    classes = len(voc_items)
 
     P.initialize_log_name(run_name, algorithm_name, description)
 
     P.write_to_log("description={}".format(description))
-    P.write_to_log("classes={}".format(classes))
     P.write_to_log("run=" + run_name)
     P.write_to_log("algorithm_name=" + algorithm_name)
 
@@ -39,17 +44,14 @@ if __name__ == "__main__":
     os.makedirs(snapshots_path, exist_ok=True)
 
     try:
-        segments_set, test_set = il.load_data(train_set_size)
-
-        train_segments_set = DataLoader(il.ImageDataset(segments_set), batch_size=5, shuffle=True)
-        print("ok")
-        test_set = DataLoader(il.ImageDataset(test_set), batch_size=5)
-        print("ok")
+        v = vl.VocDataLoader(P.voc_data_path, voc_items, P.voc_list_to_indexes(voc_items))
+        train_data_set = DataLoader(vl.VocDataset(v.train_data[0:train_set_size]), batch_size=5, shuffle=True)
+        test_data_set = DataLoader(vl.VocDataset(v.test_data[0:test_set_size]), batch_size=5)
 
         am_model = ss.build_attention_module_model(classes)
 
         P.write_to_log(am_model)
-        am_train = amt.AttentionModule(am_model, train_segments_set, test_set, classes=classes,
+        am_train = amt.AttentionModule(am_model, train_data_set, test_data_set, classes=classes,
                                        pre_train_epochs=pre_train,
                                        gpu_device=gpu,
                                        train_epochs=epochs,
