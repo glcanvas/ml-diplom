a54a72e10a7b32a30bab9d28292a662d10a5f305
glcanvas <duginetsn98@gmail.com>
2020-04-02 18:50:35 +0300

    rewrite runner

--- executor_script.py
+++ executor_script.py
@@ -8,6 +8,24 @@ import executor_nvsmi as nsmi
 import property as p
 import time
 from threading import Thread
+import threading
+import inspect
+import ctypes
+
+
+def _async_raise(tid, exctype):
+    """raises the exception, performs cleanup if needed"""
+    if not inspect.isclass(exctype):
+        raise TypeError("Only types can be raised (not instances)")
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
+    if res == 0:
+        raise ValueError("invalid thread id")
+    elif res != 1:
+        # """if it returns a number greater than one, you're in trouble,
+        # and you should call it again with exc=NULL to revert the effect"""
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
+        raise SystemError("PyThreadState_SetAsyncExc failed")
+
 
 SLEEP_SECONDS = 120
 CLASSIFIER_LEARNING_RATES = [1e-3, 1e-4, 1e-5, 1e-6]
@@ -66,7 +84,6 @@ ALGORITHM_LIST = [
 ]
 
 MAX_ALIVE_THREADS = 8
-ENV_MAX_CNT = 2
 alive_threads = Value('i', 0)
 
 
@@ -78,7 +95,7 @@ def execute_algorithm(algorithm_dict: dict, run_id: int, gpu: int, left_border: 
                                                                                            right_border,
                                                                                            TRAIN_SIZE, LOOP_COUNT,
                                                                                            classifier_learning_rate,
-                                                                                           attention_learning_rate)
+                                                                                           attention_module_learning_rate)
 
     for i in range(LOOP_COUNT):
         args = [PYTHON_EXECUTOR_NAME, script_name, '--run_name', run_name,
@@ -104,76 +121,145 @@ def execute_algorithm(algorithm_dict: dict, run_id: int, gpu: int, left_border: 
 
 def read_property() -> dict:
     return dict(
-        line.strip().split('=') for line in open(property_file) if not line.strip().startswith('#'))
+        line.strip().split('=') for line in open(property_file) if
+        not line.strip().startswith('#') and len(line.strip()) > 0)
+
+
+def build_execute_list():
+    result = []
+    run_id = RUN_NAME_RANGE_FROM
+    for left_border, right_border in CLASS_BORDER:
+        for classifier_learning_rate in CLASSIFIER_LEARNING_RATES:
+            for attention_learning_rate in ATTENTION_MODULE_LEARNING_RATES:
+                for algorithm_data in ALGORITHM_LIST:
+                    result.append((left_border, right_border, classifier_learning_rate, attention_learning_rate,
+                                   run_id, algorithm_data))
+                run_id += 1
+    return result
+
+
+def found_gpu(smi, properties: dict) -> int:
+    for k in smi['Attached GPUs']:
+        gpu = int(smi['Attached GPUs'][k]['Minor Number'])
+        free_memory = int(smi['Attached GPUs'][k]['FB Memory Usage']['Free'].split()[0])
+        if 'banned_gpu' in properties and str(gpu) in properties['banned_gpu']:
+            current_time = datetime.today().strftime('%Y-%m-%d-_-%H_%M_%S')
+            p.write_to_log("time = {}, gpu = {} is banned".format(current_time, gpu))
+            continue
+        if 'max_thread_on_gpu' in properties and smi['Attached GPUs'][k]['Processes'] is not None and \
+                len(smi['Attached GPUs'][k]['Processes']) > int(properties['max_thread_on_gpu']):
+            current_time = datetime.today().strftime('%Y-%m-%d-_-%H_%M_%S')
+            p.write_to_log("time = {}, gpu = {}, processes = {}, max processes={}".format(current_time, gpu,
+                                                                                          alive_threads.value,
+                                                                                          properties[
+                                                                                              'max_thread_on_gpu']))
+            continue
+
+        if alive_threads.value >= MAX_ALIVE_THREADS or free_memory < MEMORY_USAGE:
+            current_time = datetime.today().strftime('%Y-%m-%d-_-%H_%M_%S')
+            p.write_to_log("time = {}, gpu = {}, alive_threads = {}, free memory = {}".format(current_time,
+                                                                                              gpu,
+                                                                                              alive_threads.value,
+                                                                                              free_memory))
+            continue
+        return gpu
+    return -1
+
+
+def process_property(property_index: int, queue: list, thread_list: list, mapper_list: list, properties: dict) -> int:
+    executed_list = build_execute_list()
+
+    for prop in properties:
+        splited_prop = prop.split(".")
+        if splited_prop[0] != str(property_index):
+            p.write_to_log("prop = {}, property_index = {} SKIP".format(prop, property_index))
+            continue
+
+        index = int(properties[prop])
+        if splited_prop[1] == "add":
+            queue.append((True, executed_list[index]))
+            property_index += 1
+        if splited_prop[1] == "remove":
+            queue[index] = (False, queue[index][1])
+            property_index += 1
+        if splited_prop[1] == "stop":
+            thread_position = -1
+            for idx, i in enumerate(mapper_list):
+                if i == index:
+                    thread_position = idx
+            if thread_position == -1:
+                p.write_to_log("prop = {}, value = {} not found, skip".format(prop, index))
+            else:
+                thread_list[thread_position]._stop()
+            property_index += 1
+    return property_index
 
 
 if __name__ == "__main__":
 
     thread_list = []
+    mapper_list = []
     p.initialize_log_name("NO_NUMBER", "NO_ALGORITHM", "FOR_EXEC_PURPOSE")
     p.write_to_log("seed list: ", SEED_LIST)
-    run_id = RUN_NAME_RANGE_FROM
-    for left_border, right_border in CLASS_BORDER:
-        for classifier_learning_rate in CLASSIFIER_LEARNING_RATES:
-            for attention_learning_rate in ATTENTION_MODULE_LEARNING_RATES:
-                run_name = "RUN_{}_LEFT{}_RIGHT{}".format(run_id, left_border, right_border)
-                for algorithm_data in ALGORITHM_LIST:
 
-                    executed = False
-                    while True:
-                        smi = nsmi.NVLog()
-                        props = read_property()
+    ex_list = build_execute_list()
+    for idx, i in enumerate(ex_list):
+        p.write_to_log("idx: {} data: {}".format(idx, i))
 
-                        for k in smi['Attached GPUs']:
-                            gpu = int(smi['Attached GPUs'][k]['Minor Number'])
-                            free_memory = int(smi['Attached GPUs'][k]['FB Memory Usage']['Free'].split()[0])
-                            if str(gpu) in props['banned_gpu']:
-                                current_time = datetime.today().strftime('%Y-%m-%d-_-%H_%M_%S')
-                                p.write_to_log("time = {}, gpu = {} is banned".format(current_time, gpu))
-                                continue
-                            nduginets_env_cnt = 0
-                            if smi['Attached GPUs'][k]['Processes'] is not None:
-                                for process in smi['Attached GPUs'][k]['Processes']:
-                                    if 'nduginec_evn3' in process['Name']:
-                                        nduginets_env_cnt += 1
+    queue = [] # [(True, i) for i in ex_list]
+    execute_index = 0
+    property_index = 0
+    while execute_index < len(queue):
+        run_it, (left_border, right_border, classifier_learning_rate, attention_learning_rate, run_id,
+                 algorithm_data) = queue[execute_index]
 
-                            if alive_threads.value >= MAX_ALIVE_THREADS or free_memory < MEMORY_USAGE \
-                                    or nduginets_env_cnt >= ENV_MAX_CNT:
-                                current_time = datetime.today().strftime('%Y-%m-%d-_-%H_%M_%S')
-                                p.write_to_log(
-                                    "time = {}, alive_threads = {} nduginets_env_cnt={}".format(current_time,
-                                                                                                alive_threads.value,
-                                                                                                nduginets_env_cnt))
-                                continue
-                            executed = True
-                            # HERE IN NEW THREAD
-                            alive_threads.value += 1
-                            thread = Thread(target=execute_algorithm, args=(algorithm_data, run_id, gpu,
-                                                                            left_border, right_border,
-                                                                            classifier_learning_rate,
-                                                                            attention_learning_rate))
-                            thread.start()
-                            thread_list.append(thread)
-                            break
+        while True:
+            p.write_to_log("=" * 20)
+            properties = read_property()
 
-                        current_time = datetime.today().strftime('%Y-%m-%d-_-%H_%M_%S')
-                        if executed:
-                            p.write_to_log("time = {} success start, run_id:{} classifier_learning_rate: {},"
-                                           " attention_learning_rate: {}, left: {}, right: {}, algorithm_data: {}"
-                                           " wait: {} seconds".format(current_time,
-                                                                      run_id, classifier_learning_rate,
-                                                                      attention_learning_rate, left_border,
-                                                                      right_border,
-                                                                      algorithm_data, SLEEP_SECONDS))
-                            break
-                        p.write_to_log("time = {} can't allocate memory: {}, classifier_learning_rate: {},"
-                                       " attention_learning_rate: {}, left: {}, right: {}, algorithm_data: {}"
-                                       " wait: {} seconds".format(
-                            current_time, MEMORY_USAGE, classifier_learning_rate, attention_learning_rate, left_border,
-                            right_border, algorithm_data, SLEEP_SECONDS))
-                        time.sleep(SLEEP_SECONDS)
-                    time.sleep(SLEEP_SECONDS)
+            property_index = process_property(property_index, queue, thread_list, mapper_list, properties)
+            p.write_to_log("execute_index={}".format(execute_index))
+            p.write_to_log("property_index={}".format(property_index))
+            for idx, i in enumerate(queue):
+                p.write_to_log("idx={}, queue value={}".format(idx, i))
+            for idx, i in enumerate(zip(thread_list, mapper_list)):
+                p.write_to_log("idx={}, thread map={}".format(idx, i))
+            for idx, i in enumerate(ex_list):
+                p.write_to_log("idx: {} execute algorithm: {}".format(idx, i))
+            gpu = found_gpu(nsmi.NVLog(), properties)
+            if gpu == -1:
+                current_time = datetime.today().strftime('%Y-%m-%d-_-%H_%M_%S')
+                p.write_to_log("time = {} can't allocate memory: {}, classifier_learning_rate: {},"
+                               " attention_learning_rate: {}, left: {}, right: {}, algorithm_data: {}"
+                               " wait: {} seconds".format(current_time, MEMORY_USAGE, classifier_learning_rate,
+                                                          attention_learning_rate, left_border,
+                                                          right_border, algorithm_data, SLEEP_SECONDS))
+                time.sleep(SLEEP_SECONDS)
+                p.write_to_log("=" * 20)
+                continue
 
-                run_id += 1
+            alive_threads.value += 1
+            thread = Thread(target=execute_algorithm, args=(algorithm_data, run_id, gpu,
+                                                            left_border, right_border,
+                                                            classifier_learning_rate,
+                                                            attention_learning_rate))
+            thread.start()
+
+            thread_list.append(thread)
+            mapper_list.append(execute_index)
+
+            current_time = datetime.today().strftime('%Y-%m-%d-_-%H_%M_%S')
+            p.write_to_log("time = {} success start, run_id:{} classifier_learning_rate: {},"
+                           " attention_learning_rate: {}, left: {}, right: {}, algorithm_data: {}"
+                           " wait: {} seconds".format(current_time,
+                                                      run_id, classifier_learning_rate,
+                                                      attention_learning_rate, left_border,
+                                                      right_border,
+                                                      algorithm_data, SLEEP_SECONDS))
+            p.write_to_log("=" * 20)
+            break
+        time.sleep(SLEEP_SECONDS)
+        execute_index += 1
+
     for t in thread_list:
         t.join()
