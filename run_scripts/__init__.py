a690384d697154d7c827538ed1c52a51d806d348
nikita <duginetsn98@gmail.com>
2020-05-04 12:28:51 +0300

    fix runs

--- run_scripts/__init__.py
+++ run_scripts/__init__.py
@@ -1,5 +1,8 @@
+import sys
+print(sys.path)
+sys.path.append(0, "/home/nduginec/ml3/ml-diplom")
+
 from .executor_nvsmi import *
-from .executor_script import *
 from .initial_strategies import *
 from .local_executor import *
 from .on_server_executor import *
