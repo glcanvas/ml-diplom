db2e08eb91984e3d05fa3be6fc59edf7a7893c04
nikita <duginetsn98@gmail.com>
2020-05-04 12:41:07 +0300

    fix runs

--- model/__init__.py
+++ model/__init__.py
@@ -5,6 +5,6 @@ sys.path.insert(0, "/home/nduginec/ml3/ml-diplom")
 
 from .am_loss_function import *
 from .am_model import *
-from .googlenet import *
+# from .googlenet import *
 from .inceptionv3 import *
 from .soft_f1_loss import *
