f3b85ff46f1a42dd655f4dea5b96960fc1be9613
nikita <duginetsn98@gmail.com>
2020-05-04 14:01:22 +0300

    soft f1 loss

--- model/soft_f1_loss.py
+++ model/soft_f1_loss.py
@@ -17,11 +17,16 @@ class SoftF1Loss(nn.Module):
         """
         true_positive = (output * target).sum(axis=0)
         false_positive = (output * (1 - target)).sum(axis=0)
+        false_negative = ((1 - output) * target).sum(axis=0)
+        soft_f1_measure = 2 * true_positive / (2 * true_positive + false_negative + false_positive + 1e-16)
+        cost = 1 - soft_f1_measure
+        return cost.sum()
 
 
 if __name__ == "__main__":
     y = torch.tensor([[1.0, 0.0, 1.0], [1.0, 1.0, 0.0]])
-    y_m = torch.tensor([[0.5, 0.3, 0.8], [0.1, 0.6, 0.9]])
+    #y_m = torch.tensor([[0.5, 0.3, 0.8], [0.1, 0.6, 0.9]])
+    y_m = torch.tensor([[1, 0.0, 1], [1, 1, 0.0]])
 
     loss = SoftF1Loss()
-    loss.forward(y_m, y)
+    print(loss.forward(y_m, y))
